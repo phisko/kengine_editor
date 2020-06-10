40c0718a20f4025343845dfc001285e05a647e23
Nicolas Phister] <nicolas@phister.fr>
2020-06-10 15:20:46 +0200

    add environment plugin

---
+++ plugins/environment/EnvironmentSystem.cpp
@@ -0,0 +1,79 @@
+#include <filesystem>
+
+#include "Export.hpp"
+#include "EntityManager.hpp"
+
+#include "data/ImGuiComponent.hpp"
+#include "meta/LoadFromJSON.hpp"
+
+#include "helpers/pluginHelper.hpp"
+
+#include "json.hpp"
+#include "imgui.h"
+#include "imfilebrowser.h"
+
+kengine::EntityManager * g_em;
+
+#pragma region declarations
+static void loadScene(const char * path);
+#pragma endregion
+EXPORT void loadKenginePlugin(kengine::EntityManager & em) {
+	kengine::pluginHelper::initPlugin(em);
+
+	g_em = &em;
+
+	loadScene("resources/default_scene.json");
+
+	em += [&](kengine::Entity & e) {
+		e += kengine::ImGuiComponent([] {
+			static ImGui::FileBrowser dialog;
+			dialog.SetTitle("Load scene");
+			dialog.SetTypeFilters({ ".json" });
+
+			if (ImGui::BeginMainMenuBar())
+				if (ImGui::BeginMenu("File")) {
+					if (ImGui::MenuItem("Load scene"))
+						dialog.Open();
+					ImGui::EndMenu();
+				}
+			ImGui::EndMainMenuBar();
+
+			dialog.Display();
+
+			if (dialog.HasSelected()) {
+				loadScene(dialog.GetSelected().string().c_str());
+				dialog.ClearSelected();
+			}
+		});
+	};
+}
+
+#pragma region loadScene
+#pragma region declarations
+static void loadEntity(kengine::Entity & e, const putils::json & json);
+#pragma endregion
+static void loadScene(const char * path) {
+	static std::vector<kengine::Entity::ID> toRemove;
+
+	for (const auto id : toRemove)
+		g_em->removeEntity(id);
+	toRemove.clear();
+
+	std::ifstream f(path);
+	if (!f)
+		return;
+	const auto json = putils::json::parse(f);
+
+	for (const auto & jsonEntity : json) {
+		*g_em += [&](kengine::Entity & e) {
+			toRemove.push_back(e.id);
+			loadEntity(e, jsonEntity);
+		};
+	}
+}
+
+static void loadEntity(kengine::Entity & e, const putils::json & json) {
+	for (const auto & [_, loader] : g_em->getEntities<kengine::meta::LoadFromJSON>())
+		loader(json, e);
+}
+#pragma endregion loadScene
