427925916b5237e2bed8879305e8c6288346e0aa
Nicolas Phister <nicolas@phister.fr>
2021-01-20 12:21:42 +0100

    modelLoader -- add save

--- plugins/modelLoader/ModelLoaderSystem.cpp
+++ plugins/modelLoader/ModelLoaderSystem.cpp
@@ -5,8 +5,9 @@
 
 #include <GLFW/glfw3.h>
 
-#include "Export.hpp"
 #include "kengine.hpp"
+#include "Export.hpp"
+#include "helpers/pluginHelper.hpp"
 
 #include "data/GraphicsComponent.hpp"
 #include "data/TransformComponent.hpp"
@@ -18,18 +19,30 @@
 #include "functions/Execute.hpp"
 #include "functions/OnTerminate.hpp"
 
-#include "helpers/pluginHelper.hpp"
 #include "helpers/assertHelper.hpp"
+#include "helpers/jsonHelper.hpp"
 
 #include "imgui.h"
 #include "imfilebrowser.h"
 
+#include "magic_enum.hpp"
+#include "concat.hpp"
+#include "file_extension.hpp"
+
+using namespace kengine;
+
 static constexpr auto RECENT_FILE = "recentModels.txt";
 
 static std::list<std::string> g_recentItems;
-static ImGui::FileBrowser g_dialog;
+static ImGui::FileBrowser g_dialog(ImGuiFileBrowserFlags_CreateNewDir | ImGuiFileBrowserFlags_EnterNewFilename);
 
-using namespace kengine;
+enum class ModelAction {
+	Load,
+	Save
+};
+static ModelAction g_modelAction;
+
+static EntityID g_currentEntity = INVALID_ID;
 
 EXPORT void loadKenginePlugin(void * state) noexcept {
 	struct impl {
@@ -37,25 +50,29 @@ EXPORT void loadKenginePlugin(void * state) noexcept {
 			loadRecentItems();
 
 			entities += [](Entity & e) noexcept {
-				g_dialog.SetTitle("Load model");
-
 				e += functions::Execute{ execute };
 				e += functions::OnTerminate{ saveRecentItems };
 			};
 
-			entities += [](Entity & e) noexcept {
-				e += ImGuiMainMenuBarItemComponent{ "File", "Load model", []() noexcept {
-					if (ImGui::MenuItem("Load model"))
-						g_dialog.Open();
-				} };
-			};
+			entities += [=](Entity & e) noexcept {
+				e += ImGuiMainMenuBarItemComponent{ "File", "Model", [=]() noexcept {
+					if (ImGui::BeginMenu("Model")) {
+						for (const auto [action, name] : putils::magic_enum::enum_entries<ModelAction>()) {
+							std::string actionName(name);
+							if (ImGui::MenuItem(actionName.c_str())) {
+								g_modelAction = action;
+								g_dialog.SetTitle(std::move(actionName));
+								g_dialog.Open();
+							}
+						}
 
-			entities += [](Entity & e) noexcept {
-				e += ImGuiMainMenuBarItemComponent{ "File", "Recent models", []() noexcept {
-					if (ImGui::BeginMenu("Recent models")) {
-						for (const auto & f : g_recentItems)
-							if (ImGui::MenuItem(f.c_str()))
-								loadModel(f.c_str());
+						if (ImGui::BeginMenu("Recent")) {
+							for (const auto & f : g_recentItems)
+								if (ImGui::MenuItem(f.c_str()))
+									loadModel(f.c_str());
+							ImGui::EndMenu();
+						}
+
 						ImGui::EndMenu();
 					}
 				} };
@@ -82,37 +99,21 @@ EXPORT void loadKenginePlugin(void * state) noexcept {
 				f << s << std::endl;
 		}
 
-		static void loadModel(const char * path) noexcept {
-			static EntityID toRemove = INVALID_ID;
-
-			if (toRemove != INVALID_ID) {
-				const auto & e = entities[toRemove];
-				if (e.has<InstanceComponent>())
-					entities -= e.get<InstanceComponent>().model;
-
-				entities -= toRemove;
-			}
-
-			entities += [&](Entity & e) noexcept {
-				toRemove = e.id;
-				e += GraphicsComponent{ path };
-				e += TransformComponent{};
-				e += SelectedComponent{};
-			};
-
-			const auto it = std::find(g_recentItems.begin(), g_recentItems.end(), path);
-			if (it != g_recentItems.end())
-				g_recentItems.splice(g_recentItems.begin(), g_recentItems, it);
-			else
-				g_recentItems.push_front(path);
-		}
-
 		static void execute(float deltaTime) noexcept {
 			g_dialog.Display();
 
 			if (g_dialog.HasSelected()) {
-				loadModel(g_dialog.GetSelected().string().c_str());
+				const auto selected = g_dialog.GetSelected().string();
 				g_dialog.ClearSelected();
+
+				switch (g_modelAction) {
+				case ModelAction::Load:
+					loadModel(selected.c_str());
+					break;
+				case ModelAction::Save:
+					saveModel(selected.c_str());
+					break;
+				}
 			}
 
 			for (const auto & [e, window] : entities.with<GLFWWindowComponent>()) {
@@ -121,6 +122,75 @@ EXPORT void loadKenginePlugin(void * state) noexcept {
 					loadModel(files[0]);
 				});
 			}
+		}
+
+		static void loadModel(const char * path) noexcept {
+			if (g_currentEntity != INVALID_ID) {
+				const auto e = entities[g_currentEntity];
+				if (e.has<InstanceComponent>())
+					entities -= e.get<InstanceComponent>().model;
+				else
+					kengine_assert_failed("Entity does not have model");
+
+				entities -= e;
+			}
+
+			if (putils::file_extension(path) == "json")
+				loadFromJSON(path);
+			else {
+				entities += [&](Entity & e) noexcept {
+					g_currentEntity = e.id;
+					e += GraphicsComponent{ path };
+					e += TransformComponent{};
+					e += SelectedComponent{};
+				};
+			}
+
+			const auto it = std::find(g_recentItems.begin(), g_recentItems.end(), path);
+			if (it != g_recentItems.end())
+				g_recentItems.splice(g_recentItems.begin(), g_recentItems, it);
+			else
+				g_recentItems.push_front(path);
+		}
+
+		static void loadFromJSON(const char * path) noexcept {
+			std::ifstream f(path);
+			if (!f) {
+				kengine_assert_failed(putils::concat("Failed to open '", path, "'"));
+				return;
+			}
+
+			const auto modelJSON = putils::json::parse(f);
+			const auto model = jsonHelper::createEntity(modelJSON);
+			entities += [&](Entity & e) noexcept {
+				g_currentEntity = e.id;
+				e += InstanceComponent{ model.id };
+				e += GraphicsComponent{};
+				e += TransformComponent{};
+				e += SelectedComponent{};
+			};
+		}
+
+		static void saveModel(const char * path) noexcept {
+			if (g_currentEntity == INVALID_ID)
+				return;
+
+			std::ofstream f(path);
+			if (!f) {
+				kengine_assert_failed(putils::concat("Could not open '", path, "' for writing"));
+				return;
+			}
+
+			const auto e = entities[g_currentEntity];
+			const auto instance = e.tryGet<InstanceComponent>();
+			if (!instance) {
+				kengine_assert_failed("Entity does not have model");
+				return;
+			}
+			const auto model = entities[instance->model];
+
+			const auto json = jsonHelper::saveEntity(model);
+			f << json.dump(4);
 		}
 	};
 
